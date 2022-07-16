973e52702e1a46e4d8c5239437dcb0f2f002d085
BrayanUTH <10brayanalvarez@gmail.com>
2022-07-16 13:20:34 -0600

    Agregar archivos de proyecto.

---
+++ PM2E2GRUPO1/Models/Sitio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM2E2GRUPO1.Models
+{
+    public class Sitio
+    {
+        public int id { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public String descripcion { get; set; }
+        //public String pathImage { get; set; }
+        public Byte[] image { get; set; }
+        public Byte[] audioFile { get; set; }
+    }
+}
